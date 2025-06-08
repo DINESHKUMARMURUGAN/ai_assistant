23a58bb1a6f960d8a1f19ec5c16db7773dd1ee4a
Dineshkumar Murugan <mdineshkumar90@karunya.edu.in>
2025-06-08 09:35:36 -0400

    WIP

--- src/ai_assistant/load_llm/load_llm.py
+++ src/ai_assistant/load_llm/load_llm.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-
+import torch
 def load_model(model_name="mistralai/Mistral-7B-Instruct-v0.1"):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, use_safetensors=True,device_map="auto",torch_dtype=torch.float16)
     generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
     return generator
--- src/ai_assistant/main.py
+++ src/ai_assistant/main.py
@@ -1,10 +1,10 @@
-from load_llm.load_llm import load_model
+from ai_assistant.load_llm import load_model
 
 def main():
-    model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+    model_name = "HuggingFaceTB/SmolLM2-1.7B-Instruct"# "sshleifer/tiny-gpt2"
     generator = load_model(model_name)
     prompt = "What is the capital of France?"
-    output = generator(prompt, max_length=50)
+    output = generator(prompt, max_new_tokens=500)
     print("Generated Text:", output[0]['generated_text'])
 
 if __name__ == "__main__":
