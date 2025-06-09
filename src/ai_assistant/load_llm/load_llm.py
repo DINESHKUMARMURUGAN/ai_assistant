c0eadb84a6310ab010d2083c5d95541fc02e031f
foo <mdineshkumar90@karunya.edu.in>
2025-06-08 23:12:18 -0400

    Added gradio based app

--- src/ai_assistant/load_llm/load_llm.py
+++ src/ai_assistant/load_llm/load_llm.py
@@ -2,6 +2,18 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
 def load_model(model_name="mistralai/Mistral-7B-Instruct-v0.1"):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, use_safetensors=True,device_map="auto",torch_dtype=torch.float16)
-    generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        trust_remote_code=True,
+        use_safetensors=True,
+        device_map="auto",
+        torch_dtype=torch.float16
+    )
+    generator = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=False  # Optional: can also be set per call
+    )
     return generator
+
